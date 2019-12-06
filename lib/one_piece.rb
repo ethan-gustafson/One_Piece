755064dbea4b642694e94a1b68d81328660614f3
Ethan <ethan_gustafson@outlook.com>
2019-12-05 21:13:39 -0500

    Saving

--- lib/one_piece.rb
+++ lib/one_piece.rb
@@ -1,4 +1,11 @@
+require 'pry'
+require 'open-uri'
+require 'nokogiri'
+
 require "one_piece/version"
+require "one_piece/characters.rb"
+require "one_piece/scraper.rb"
+require "one_piece_cli.rb"
 
 module OnePiece
   class Error < StandardError; end
