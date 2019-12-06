973acafe4c612f684d697c44d984741d7d718be2
Ethan <ethan_gustafson@outlook.com>
2019-12-06 02:31:10 -0500

    new methods

--- lib/classes/scraper.rb
+++ lib/classes/scraper.rb
@@ -18,34 +18,62 @@ class Scraper
         html = "https://onepiece.fandom.com/wiki/Haki"
     end
 
-    def self.all_characters
+    def summary
         url = open(self.char_page)
         page = Nokogiri::HTML(url)
-        #onepiece_bio = page.css(".mw-content-ltr p")[0].text - iteration with name and bio all at once 
-        #onepiece_bio.chomp("\n").delete"\""
-        characters_array = page.css(".mw-headline")
-        characters_array[2..90].each_with_index do |character, index|
-            puts "#{index}. #{character.name}"
-        #c = Characters.new every iteration creates new character, assign bio same time
-    end 
+        sorted = page.css(".mw-parser-output").text.split(".")
+        rejoin = sorted.slice(3..12).join(" ")
+        summary = rejoin
+        # summary = sorted[3..12]
+        # summary.text
+    end
 
-    def fruit_types_list
+    def all_characters
+        url = open(self.char_page)
+        page = Nokogiri::HTML(url)
+        characters_array = page.css(".mw-headline")
+            characters_array[2..90].each.with_index(1) do |character,index|
+                puts "#{index}. #{character.attributes.values[1]}"
+            end
+    end
+
+    def bios
+        url = open(self.char_page)
+        page = Nokogiri::HTML(url)
+        sorted = page.css(".mw-parser-output").text.split(".")
+            sorted.each do |bio|
+                puts bio
+            end.text
+    end
+
+            #onepiece_bio = page.css(".mw-content-ltr p")[0].text - iteration with name and bio all at once 
+        #onepiece_bio.chomp("\n").delete"\""
+        #c = Characters.new every iteration creates new character, assign bio same time
+    
+
+    #def fruit_types_list
+     #   url = open(self.fruits_page)
+      #  page = Nokogiri::HTML(url)
+       # logia_fruit = page.css("p.italic.bolder")[1].text.chomp(" -")
+        #paramecia_fruit = page.css("p.italic.bolder")[13].text.chomp("-")
+        #zoan_fruit = ("p.italic.bolder")[19].text.chomp.(" -")
+    #end #Done - I dont think I need this method just to scrape three word. -_-
+
+    def all_fruits 
         url = open(self.fruits_page)
         page = Nokogiri::HTML(url)
-        logia_fruit = page.css("p.italic.bolder")[1].text.chomp(" -")
-        paramecia_fruit = page.css("p.italic.bolder")[13].text.chomp("-")
-        zoan_fruit = ("p.italic.bolder")[19].text.chomp.(" -")
-
         devil_fruits = page.css(".content.rich-content.article")[0].text.strip.split("•")
         devil_fruits.shift
         devil_fruits
-    end #Done
+    end
 
     def haki_types
-
+        url = open(self.haki_page)
+        page = Nokogiri::HTML(url)
     end
 
 end
+binding.pry
 
 #connecting the href link
 #page.css("a").attr("href").value
