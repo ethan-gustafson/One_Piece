cfe049e5fe64bf6bad54d9e0031e35ae35fa9a34
Ethan <ethan_gustafson@outlook.com>
2020-05-05 18:40:31 -0400

    removed replit readme section

--- vendor/cache /ruby/2.6.0/gems/nokogiri-1.10.8/ext/nokogiri/tmp/x86_64-apple-darwin19/ports/libxml2/2.9.10/libxml2-2.9.10/xml2Conf.sh
+++
@@ -1,8 +0,0 @@
-#
-# Configuration file for using the XML library in GNOME applications
-#
-XML2_LIBDIR="-L/Users/user/Desktop/Flatiron/Projects/CLI/vendor/cache /ruby/2.6.0/gems/nokogiri-1.10.8/ports/x86_64-apple-darwin19/libxml2/2.9.10/lib"
-XML2_LIBS="-lxml2 -lz  -lpthread  -liconv  -lm "
-XML2_INCLUDEDIR="-I/Users/user/Desktop/Flatiron/Projects/CLI/vendor/cache /ruby/2.6.0/gems/nokogiri-1.10.8/ports/x86_64-apple-darwin19/libxml2/2.9.10/include/libxml2"
-MODULE_VERSION="xml2-2.9.10"
-
