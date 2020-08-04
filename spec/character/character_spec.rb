88983c4e71168f68bd90a93229f8001c3cff0ec4
Ethan <ethan_gustafson@outlook.com>
2020-08-04 03:34:03 -0400

    put specs into their own directories and updated relative paths in both character/devil fruit

--- spec/character/character_spec.rb
+++ spec/character/character_spec.rb
@@ -1,20 +1,20 @@
-require_relative '../lib/classes/devil_fruit.rb'
+require_relative '../../lib/classes/character.rb'
 require 'shared_examples/character_devil_fruit'
 
-describe DevilFruit do
+describe Character do
 
     context "attributes" do
 
-        subject { DevilFruit.new("Logia", "https://onepiece.fandom.com/wiki/Logia") } 
+        subject { Character.new("Luffy", "https://onepiece.fandom.com/wiki/Monkey_D._Luffy") } 
 
         it "Instantiate with a name & URL" do
-            expect(subject).to have_attributes(:name => "Logia", :url => "https://onepiece.fandom.com/wiki/Logia")
+            expect(subject).to have_attributes(:name => "Luffy", :url => "https://onepiece.fandom.com/wiki/Monkey_D._Luffy")
         end
 
         it_behaves_like('character_devil_fruit')
 
         it ":name, :url & :bio must be strings" do
-            subject.bio = "This type allows the user to completely alter their form into a natural element and manipulate it at will."
+            subject.bio = "Kaizoku-ō ni ore wa naru!"
             expect(subject.name).to be_an(String)
             expect(subject.url).to be_an(String)
             expect(subject.bio).to be_an(String)
@@ -24,7 +24,7 @@ describe DevilFruit do
 
     context 'class Methods' do
         it '.all method which will record all instances of the class' do
-            expect(DevilFruit.all).to be_an(Array)
+            expect(Character.all).to be_an(Array)
         end
     end
     
