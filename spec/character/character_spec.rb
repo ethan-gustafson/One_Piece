32c18a24b17b023bb81e2bd041eaff26ce3870c6
Ethan <ethan_gustafson@outlook.com>
2020-10-07 18:49:25 -0400

    changed tab indentation in character.rb/character_spec.rb & added nokogiri/open-uri to spec_helper

--- spec/character/character_spec.rb
+++ spec/character/character_spec.rb
@@ -2,29 +2,32 @@ require_relative "../spec_helper"
 
 describe Character do
 
-    context "attributes" do
+  context "attributes" do
+    # Subject is just the starting describe argument name
+    subject { Character.new("Luffy", "https://onepiece.fandom.com/wiki/Monkey_D._Luffy") } 
 
-        subject { Character.new("Luffy", "https://onepiece.fandom.com/wiki/Monkey_D._Luffy") } # Subject is just the starting describe argument name
-
-        it "Instantiate with a name & URL" do
-            expect(subject).to have_attributes(:name => "Luffy", :url => "https://onepiece.fandom.com/wiki/Monkey_D._Luffy")
-        end
-
-        it_behaves_like('character_devil_fruit') # this will essentially 'copy' and 'paste' the code in character_devil_fruit right here
-
-        it ":name, :url & :bio must be strings" do
-            subject.bio = "Kaizoku-ō ni ore wa naru!"
-            expect(subject.name).to be_an(String)
-            expect(subject.url).to be_an(String)
-            expect(subject.bio).to be_an(String)
-        end
-        
+    it "Instantiate with a name & URL" do
+      expect(subject).to have_attributes(
+        name: "Luffy", 
+        url: "https://onepiece.fandom.com/wiki/Monkey_D._Luffy"
+      )
     end
 
-    context 'class Methods' do
-        it '.all method which will record all instances of the class' do
-            expect(Character.all).to be_an(Array)
-        end
+    # it_behaves_like will essentially 'copy' and 'paste' the code in character_devil_fruit right here
+    it_behaves_like('character_devil_fruit')
+
+    it ":name, :url & :bio must be strings" do
+      subject.bio = "Kaizoku-ō ni ore wa naru!"
+      expect(subject.name).to be_an(String)
+      expect(subject.url).to be_an(String)
+      expect(subject.bio).to be_an(String)
     end
-    
+
+  end
+
+  context 'class Methods' do
+    it '.all method which will record all instances of the class' do
+      expect(Character.all).to be_an(Array)
+    end
+  end
 end
