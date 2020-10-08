2a85646d6f399893ed99a002f270b5e7fd519c03
Ethan <ethan_gustafson@outlook.com>
2020-10-08 14:04:43 -0400

    added pending tests to menu_spec.rb

--- spec/cli/menu_spec.rb
+++ spec/cli/menu_spec.rb
@@ -1,4 +1,11 @@
 require_relative "../spec_helper.rb"
-# describe Menu do
-  
-# end
+describe Menu do
+  describe "instance methods" do
+    it "#initialize"
+    it "#user_input"
+    it "#list_options"
+    it "#options"
+    it "#fruit_menu"
+    it "#character_menu"
+  end
+end
