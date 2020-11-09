9072e9a4522df7b43232bf25c746cbe22bdb0571
Ethan <ethan_gustafson@outlook.com>
2020-11-09 16:31:23 -0500

    remove extra helper method already written in scraper_helper.rb, update changes to reflect new class additions in lib/classes

--- data/scripts/scraped_data.rb
+++ data/scripts/scraped_data.rb
@@ -1,35 +1,30 @@
-require 'require_all'; require 'nokogiri'; require 'open-uri'
-require 'pry'; require_rel ("../../lib")
-
-class ScrapedData < Scraper
+require 'require_all'
+require 'nokogiri'
+require 'open-uri'
+require 'colorize'
+require 'pry'
+require_relative './scraper_helper.rb'
+require_rel ("../../lib")
+class ScrapedData
 
   def initialize
-    super
+    Character.get_all_characters
+    DevilFruit.get_all_fruits
     write_strings_to_files
     write_arrays_to_files
   end
 
   def write_strings_to_files
-    File.write(File.join(".", "data", "episodes.txt"), @episode_list)
-    File.write(File.join(".", "data", "fruits_info.txt"), @fruits_info)
-    File.write(File.join(".", "data", "haki.txt"), @haki)
-    File.write(File.join(".", "data", "summary.txt"), @summary)
+    File.write(File.join("data", "episodes.txt"), OnePiece.episodes)
+    File.write(File.join("data", "fruits_info.txt"), DevilFruit.fruits_summary)
+    File.write(File.join("data", "haki.txt"), OnePiece.haki_explanation)
+    File.write(File.join("data", "summary.txt"), OnePiece.summary)
   end
 
   def write_arrays_to_files
-    File.write(File.join(".", "data", "characters.txt"), obj_to_arr_of_hashes(@characters))
-    File.write(File.join(".", "data", "fruits.txt"), obj_to_arr_of_hashes(@fruits))
-  end
-
-  def obj_to_arr_of_hashes(obj)
-    arr = []
-    obj.each do |e|
-      hash = {}
-      e.instance_variables.each do |v|
-        hash[v[1..-1].to_sym] = e.instance_variable_get(v)
-      end
-      arr.push(hash)
-    end
-    arr
+    File.write(File.join("data", "characters.txt"), ScraperHelper.obj_to_arr_of_hashes(Character.all))
+    File.write(File.join("data", "fruits.txt"), ScraperHelper.obj_to_arr_of_hashes(DevilFruit.all))
   end
 end
+
+ScrapedData.new
