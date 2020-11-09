017ade80e71fd957caef2dc9d12a285251f62d35
Ethan <ethan_gustafson@outlook.com>
2020-11-09 16:29:49 -0500

    create one piece class, with .summary, .episodes, .haki_explanation, .where_you_can_watch class methods

---
+++ lib/classes/one_piece.rb
@@ -0,0 +1,17 @@
+class OnePiece
+  def self.summary
+    OnePieceScraper.summary
+  end
+
+  def self.episodes
+    OnePieceScraper.episodes
+  end
+
+  def self.haki_explanation
+    OnePieceScraper.haki
+  end
+
+  def self.where_you_can_watch
+    "\nYou can find One Piece on Crunchyroll, Funimation or Hulu!"
+  end
+end
