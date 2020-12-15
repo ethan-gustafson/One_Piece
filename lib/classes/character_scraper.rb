492dda580390046bf62297f9a251f1b359d63b70
Ethan <ethan_gustafson@outlook.com>
2020-12-15 08:01:42 -0500

    add comments detailing methods, include start_i_end_i methods for simple character identification

--- lib/classes/character_scraper.rb
+++ lib/classes/character_scraper.rb
@@ -2,9 +2,9 @@ require_relative "../modules/scraper.rb"
 class CharacterScraper
   extend Scraper::ClassMethods
 
-  # self.characters is responsible for scraping character information. It will
+  # `self.characters` is responsible for scraping character information. It will
   #   be handling xml. Calling .text on nil xml results with a NoMethodError output,
-  #   effectively breaking the application. First I'm esting if the xml is valid,
+  #   effectively breaking the application. First I'm testing if the xml is valid,
   #   and if it isn't, old data will be read through the  `display_previous_data`
   #   method. If it is valid, lets pass the xml to the next method, in order to
   #   create the characters for the Character class.
@@ -23,7 +23,7 @@ class CharacterScraper
     return character_nodes
   end
 
-  # self.create_characters is responsible for attaching attributes to each instance.
+  # `self.create_characters` is responsible for attaching attributes to each instance.
   #   First I take the characters argument, iterate over it with each item and its
   #   index. The character url is set, a new character instance is generated with
   #   their name and url. Next, I call `set_character_start_and_end_index`, and 
@@ -38,43 +38,89 @@ class CharacterScraper
     end
   end
 
-  # `set_character_start_and_end_index` is responsible for pinpointing where a
+  # `self.set_character_start_and_end_index` is responsible for pinpointing where a
   #   character's bio starts and ends. This is very hardcoded, and requires
   #   tedious fixing when navigating back to character wiki pages.
 
   def self.set_character_start_and_end_index(character, index)
     case index
-    when 0 # 0. Monkey D. Luffy
-      character.start_i = 6
-      character.end_i = 7
-    when 1, 5  # 1. Roronoa Zoro, 5. Tony Tony Chopper
-      character.start_i = 5  
-      character.end_i = 6 
-    when 2 # 2. Nami
-      character.start_i = 4
-      character.end_i = 5 
-    when 4 # 4. Sanji
-      character.start_i = 5  
-      character.end_i = 8 
-    when 3 # 3. Usopp
-      character.start_i = 7
-      character.end_i = 8 
-    when 6 # 6. Nico Robin
-      character.start_i = 8
-      character.end_i = 9 
-    when 7 # 7. Franky
-      character.start_i = 5
-      character.end_i = 6 
-    when 8 # 8. Brook
-      character.start_i = 5
-      character.end_i = 7 
-    when 9 # 9. Jinbe
-      character.start_i = 2
-      character.end_i = 4 
+    when 0
+      set_luffy_start_i_end_i(character)
+    when 1, 5 
+      set_zoro_start_i_end_i(character)
+      set_chopper_start_i_end_i(character)
+    when 2
+      set_nami_start_i_end_i(character)
+    when 4
+      set_sanji_start_i_end_i(character)
+    when 3
+      set_usopp_start_i_end_i(character)
+    when 6
+      set_robin_start_i_end_i(character)
+    when 7
+      set_franky_start_i_end_i(character)
+    when 8
+      set_brook_start_i_end_i(character)
+    when 9
+      set_jinbe_start_i_end_i(character)
     end
   end
 
-  # set_bio is responsible for scraping a specific characters bio information.
+  # The set_character_start_i_end_i methods are used in order to easily identify 
+  #   whose information needs to be changed based on where it moved in the 
+  #   wiki page.
+
+  def set_luffy_start_i_end_i(character)
+    character.start_i = 6
+    character.end_i = 7
+  end
+
+  def set_zoro_start_i_end_i(character)
+    character.start_i = 5
+    character.end_i = 6
+  end
+
+  def set_chopper_start_i_end_i(character)
+    character.start_i = 5
+    character.end_i = 6
+  end
+
+  def set_nami_start_i_end_i(character)
+    character.start_i = 4
+    character.end_i = 5 
+  end
+
+  def set_sanji_start_i_end_i(character)
+    character.start_i = 5  
+    character.end_i = 8 
+  end
+
+  def set_usopp_start_i_end_i(character)
+    character.start_i = 7
+    character.end_i = 8 
+  end
+
+  def set_robin_start_i_end_i(character)
+    character.start_i = 8
+    character.end_i = 9 
+  end
+
+  def set_franky_start_i_end_i(character)
+    character.start_i = 5
+    character.end_i = 6 
+  end
+
+  def set_brook_start_i_end_i(character)
+    character.start_i = 5
+    character.end_i = 7 
+  end
+
+  def set_jinbe_start_i_end_i(character)
+    character.start_i = 2
+    character.end_i = 4 
+  end
+
+  # self.set_bio is responsible for scraping a specific characters bio information.
   #   Each character has their own fandom page. By setting the character start_i
   #   and end_i in the previous method, we can now set where the character bio
   #   will start and end depending on where it is on the page. If the scrape isn't
