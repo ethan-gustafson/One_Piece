8d1dbaad0ecb052ed7d69245f46a7db80095a0fa
Ethan <ethan_gustafson@outlook.com>
2020-12-22 14:36:09 -0500

    move required gems into one array, load modules before classes in environment.rb

--- config/environment.rb
+++ config/environment.rb
@@ -1,10 +1,11 @@
+#!/usr/bin/env ruby
 require 'bundler'
 Bundler.require
 
-require 'nokogiri'
-require 'open-uri'
-require 'colorize'
-require 'pry'
-require 'readline'
+# require necessary gems & paths
+['nokogiri', 'open-uri', 'colorize', 'readline', 'pry'].each do |gem|
+  require gem
+end
 
-require_all 'lib'
+require_all 'lib/modules'
+require_all 'lib/classes'
